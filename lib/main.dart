f04cc713bee037236642408f941f4f8022d79f44
Merlin <abdalaziz.02@proton.me>
2025-12-22 23:19:48 +0300

    Red screen fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+// pages
 import 'package:project_streaker/pages/myhomepage.dart';
+// providers
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +15,26 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+    final List<SingleChildWidget> providers = [
+      // Add your providers here
+    ];
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    final Widget app = MaterialApp(
+        title: 'Streaker Demo',
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+      // ),
+      // darkTheme: ThemeData.dark(
+      //   useMaterial3: true,
+      // ),
+      
+      home: const MyHomePage(),
+      routes: {
+        '/home': (context) => const MyHomePage(),
+      },
+      initialRoute: '/home',
     );
+
+    return providers.isNotEmpty ? MultiProvider(providers: providers, child: app) : app;
   }
 }
